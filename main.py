34f8ed152b65c56331f3e8ac244f37b562dbea8a
Benjamin Slater <benjamin.ed.slater@gmail.com>
2021-07-25 18:10:09 -0700

    added sum noise

--- main.py
+++ main.py
@@ -20,9 +20,18 @@ def normalize(p, new_max):
 
     return p
 
-def random_noise(size, filename, cluster):
+def random_noise(size, filename, cluster, imgs, norms):
     # generate random image matrix
     p = [[random.randint(0, 255) for c in range(size)] for r in range(size)]
+
+    # sum multiple random images to create larger groups
+    for img in range(imgs - 1):
+        if (img + 1) % 5 == 0:
+            print('generating image:', img + 1)
+        img = [[random.randint(0, 255) for c in range(size)] for r in range(size)]
+        for row in range(len(p)):
+            for col in range(len(p[row])):
+                p[row][col] += img[row][col]
 
     # avg pixels in clusters to form larger groups
     if cluster:
@@ -35,7 +44,7 @@ def random_noise(size, filename, cluster):
                 sum_value = 0
                 for offset in range(cluster):
                     sum_value = sum_value + np.sum(p[row + offset][col:col + cluster])
-                avg_value = round(sum_value / 64)
+                avg_value = round(sum_value / (cluster * cluster))
 
                 # update all values to use average
                 for row_offset in range(cluster):
@@ -43,7 +52,8 @@ def random_noise(size, filename, cluster):
                         p[row + row_offset][col + col_offset] = avg_value
 
     # normalize values to increase range
-    p = normalize(p, 255)
+    for norm in range(norms):
+        p = normalize(p, 255)
 
     w = png.Writer(size, size, greyscale=True)
     f = open(filename, 'wb')
@@ -64,12 +74,16 @@ if __name__ == "__main__":
                         help='seed to use for generation')
     parser.add_argument('--cluster', type=int, default=0,
                         help='width of kernal to average neighbouring pixels')
+    parser.add_argument('--imgs', type=int, default=1,
+                        help='number of random generated images to combine')
+    parser.add_argument('--norms', type=int, default=1,
+                        help='number of times to normalize image')
 
     args = parser.parse_args()
 
     random.seed(args.seed) # set seed if included
     # Implement specified noise generator
     if args.gentype == 'random':
-        random_noise(args.size, args.filename, args.cluster)
+        random_noise(args.size, args.filename, args.cluster, args.imgs, args.norms)
     else:
         raise ValueError('Noise generator "{0}" does not exist. Run --help to see supported generators.'.format(args.gentype))
