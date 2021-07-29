eab701966c0e2dbb1eb8b37e2a96604719b4919b
Benjamin Slater <benjamin.ed.slater@gmail.com>
2021-07-28 19:15:11 -0700

    reworked packaging

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="noise_img_generator",
-    version="1.0.0",
+    version="1.0.5",
     author="Benjamin Slater",
     author_email="benjamin.ed.slater@gmail.com",
     description="CLI to generate noise images for use in game design",
@@ -14,17 +14,22 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/slaterb1/noise_img_generator",
     project_urls={
-        "Bug Tracker": "https://github.com/slaterb1/noise_image_generator/issues",
+        "Bug Tracker": "https://github.com/slaterb1/noise_img_generator/issues",
+        "Source": "https://github.com/slaterb1/noise_img_generator"
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "noise_img_generator"},
-    packages=setuptools.find_packages(where="noise_img_generator"),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "numpy",
+        "pypng"
+    ],
     python_requires=">=3.6",
     entry_points='''
             [console_scripts]
-            noise_img_generator=noise_img_generator.cli
+            noise_img_generator=noise_img_generator.__main__:cli
         '''
 )
